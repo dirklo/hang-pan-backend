0cd4e78ceb99be14cd443709d59b32ccbc49bbd6
Rick <dirklo@gmail.com>
2021-04-10 19:05:59 +0200

    adds validations

--- app/models/scale.rb
+++ app/models/scale.rb
@@ -4,4 +4,13 @@ class Scale < ApplicationRecord
     scope :alphabetical, -> {order(:name)}
     validates :name, presence: true
     validates :name, uniqueness: true
+    validate :clean_name
+    validates_length_of :name, maximum: 20
+
+    def clean_name
+        profanity_filter = LanguageFilter::Filter.new matchlist: :profanity
+        if profanity_filter.match? name then
+          errors.add(:name, "The following language is inappropriate in a username: #{profanity_filter.matched(name).join(', ')}")
+        end
+    end
 end
